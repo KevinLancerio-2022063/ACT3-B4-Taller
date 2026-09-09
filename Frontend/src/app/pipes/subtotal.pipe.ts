ab088b166f3a5270e17f85438b10e0e9469ac667
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-09-09 00:32:09 -0600

    feat: Se ha agregado un método que transforma los datos recibidos y retorna el subtotal

--- Frontend/src/app/pipes/subtotal.pipe.ts
+++ Frontend/src/app/pipes/subtotal.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
   standalone: true
 })
 export class SubtotalPipe implements PipeTransform {
-  // Metodo que transforma los datos recibidos
+  // Método que transforma los datos recibidos
   // Recibe el precio y la cantidad, y retorna el subtotal
   transform(precio: number, cantidad: number): number {
     if (!precio || !cantidad) {
